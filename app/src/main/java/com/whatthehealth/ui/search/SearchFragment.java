a6c43cefaf3831549779c5954e7d3110afa5f3b8
Ania Wójcik <34057261+wojcikania12@users.noreply.github.com>
2020-06-17 16:04:45 +0200

    get data from api

--- app/src/main/java/com/whatthehealth/ui/search/SearchFragment.java
+++ app/src/main/java/com/whatthehealth/ui/search/SearchFragment.java
@@ -13,22 +13,52 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.whatthehealth.R;
+import com.whatthehealth.models.FoundRecipe;
+import com.whatthehealth.network.ClientInstance;
+import com.whatthehealth.network.GetFoundRecipesService;
+import com.whatthehealth.ui.shoppinglist.ShoppingListViewModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SearchFragment extends Fragment {
-
     private SearchViewModel searchViewModel;
+    private String ingredients = "apple";
+    private String api_key = "c4bcf0cd65d0487b98e3f168328eaef7";
 
+    private void generateDataList(List<FoundRecipe> recipeList) {
+        for(FoundRecipe recipe : recipeList){
+            System.out.println(recipe.getTitle());
+        }
+    }
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         searchViewModel =
                 ViewModelProviders.of(this).get(SearchViewModel.class);
         View root = inflater.inflate(R.layout.fragment_search, container, false);
         final TextView textView = root.findViewById(R.id.text_search);
-        searchViewModel.getText().observe(this, new Observer<String>() {
+        searchViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
                 getActivity().setTitle("Found recipes");
+            }
+        });
+        GetFoundRecipesService service = ClientInstance.getRetrofitInstance().create(GetFoundRecipesService.class);
+        Call<List<FoundRecipe>> call = service.getAllRecipes(ingredients,api_key);
+        call.enqueue(new Callback<List<FoundRecipe>>() {
+            @Override
+            public void onResponse(Call<List<FoundRecipe>> call, Response<List<FoundRecipe>> response) {
+                if(response.isSuccessful()){
+                    generateDataList(response.body());
+                }
+            }
+            @Override
+            public void onFailure(Call<List<FoundRecipe>> call, Throwable t) {
+                System.out.println(t);
             }
         });
         return root;
